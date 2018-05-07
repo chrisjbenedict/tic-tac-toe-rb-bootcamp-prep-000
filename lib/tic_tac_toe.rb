df8aa8a929e8f6903d608a09ed1c534cce4f4e66
Christopher Benedict <chrisjbenedict@gmail.com>
2018-05-07 18:26:23 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,81 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]}"
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]}"
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]}"
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def valid_move?(board_index)
+  if index.between?(1,9) && !position_taken?(board,index)
+    true
+  end
+  index.between?(0,8) && !position_taken?(board,index)
+end
+
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets
+  index = input_to_index(input)
+  if valid_move?(board,index)
+    move(board,index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def play(board)
+  turn_counter = 0
+  while turn_counter < 9
+    turn(board)
+    turn_counter += 1
+  end
+end
+
+def turn_count(board)
+  turns = 0
+  board.each do |entry|
+    if entry == "X" || entry == "O"
+      turns += 1
+    end
+  end
+  turns
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
+
+    
